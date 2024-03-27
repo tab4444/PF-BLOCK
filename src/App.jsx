76306b9cef710fa3fa9496f9507a6992b3276704
tab4444 <thiagoblock445@gmail.com>
2024-03-27 19:33:21 -0300

    firestore

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,57 @@ import ItemDetailContainer from "./components/ItemDetailContainer";
 import Cart from "./components/Cart";
 import CartProvider from "./context/CartContext";
 
+import { useState } from "react";
+
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 
+import { getFirestore, doc, getDoc, collection, getDocs, query, where} from "firebase/firestore"
+import { useEffect } from "react";
+
 function App() {
+  //coleccion
+  const [products, setProducts] = useState([])
+  useEffect(()=> {
+    const db = getFirestore()
+    const  itemsCollection = collection(db, "productos")
+    getDocs(itemsCollection).then((snapshot) =>{
+      setProducts(snapshot.docs.map((doc)=>(
+        {id:doc.id,...doc.data()}
+      )))
+    })
+  })
+
+  //coleccion filtrada
+  const [productos, setProductos] = useState([])
+  useEffect(()=>{
+    const db = getFirestore()
+    const q = query(
+      collection(db,"productos"),
+      where("categoria", "==", "monitor")
+    )
+    getDocs(q).then((snapshot)=>{
+
+      if(snapshot.size === 0){
+        console.log("No hay resultado")
+      }
+      setProductos(snapshot.docs.map((doc)=>(
+        {id: doc.id,...doc.data()}
+      )))
+    })
+  })
+
+  // documento
+  const [product,setProduct] = useState(null);
+  useEffect(()=> {
+    const db = getFirestore();
+    const productRef = doc(db, "productos", "0VvO8h4pTR38SFzSepYW")
+    getDoc(productRef).then((snapshot) => {
+      if(snapshot.exists){
+        setProduct({id: snapshot.id, ...snapshot.data()})
+      }
+    })
+  })
+
   return (
     <>
       <BrowserRouter>
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAqIZ-o6UCANgv537GjSejWBtawqLplTXo",
+  authDomain: "blockelectronicos.firebaseapp.com",
+  projectId: "blockelectronicos",
+  storageBucket: "blockelectronicos.appspot.com",
+  messagingSenderId: "728762467192",
+  appId: "1:728762467192:web:63cdbd594895baa0bb3e8d"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
